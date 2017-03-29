ba0db5adf1246745a429d0f3b9cb4853d32382c6
jcoder <106833215@qq.com>
2017-03-29 22:43:44 +0800

    【修复】修改配置

--- src/main/java/com/netposa/bootpro/web/DataController.java
+++ src/main/java/com/netposa/bootpro/web/DataController.java
@@ -1,16 +1,20 @@
 package com.netposa.bootpro.web;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netposa.bootpro.entites.BaseResponse;
 import com.netposa.bootpro.service.DataService;
 import com.netposa.bootpro.utils.ConstUtil;
-import com.netposa.datapro.entites.BaseResponse;
 
 @RestController("/")
 public class DataController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(DataController.class);
     
     @Autowired
     private DataService dataService;
@@ -27,6 +31,18 @@ public class DataController {
     
     @GetMapping("/database/str")
     public String getDataBaseRandomStr() {
+        return dataService.queryRandom();
+    }
+    
+    @GetMapping("/thread")
+    public String threadTest() {
+    	LOG.info("thread:" + Thread.currentThread().getName());
+    	
+    	try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
         return dataService.queryRandom();
     }
 }
